b0bce06cb34dcdf6cdc4118a09079bfd76778bd5
Jian Beerens <beerensjian@gmail.com>
2023-11-21 18:53:59 +0100

    Updated list index

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -1,5 +1,10 @@
 <x-app>
-    @foreach($animes as $anime)
-        {{ $anime->title }}
-    @endforeach
+    <div class="row mt-5">
+        <div class="col-3 bg-body-secondary me-4">
+            settings tab
+        </div>
+        <div class="col bg-body-secondary">
+            list of animes
+        </div>
+    </div>
 </x-app>
