14643c9db3b91de11510318d0249d205342dfd2d
Jian Beerens <beerensjian@gmail.com>
2023-11-21 17:11:21 +0100

    Added image_url property to anime

--- database/migrations/2023_11_18_121933_create_animes_table.php
+++ database/migrations/2023_11_18_121933_create_animes_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->string('image_url')->nullable();
             $table->unsignedInteger('rating')->nullable();
             $table->unsignedInteger('episode')->default(1);
             $table->unsignedInteger('total_episodes')->nullable();
