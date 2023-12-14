bad97e958fd7cebd3e917d67bf5fe0cb348eda6a
Jian Beerens <beerensjian@gmail.com>
2023-12-14 21:17:39 +0100

    Relationship between user & listItem

--- app/Models/ListItem.php
+++ app/Models/ListItem.php
@@ -6,6 +6,7 @@ use App\Enum\AnimeStatus;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ListItem extends Model
@@ -16,7 +17,6 @@ class ListItem extends Model
     protected $casts = [
         'status' => AnimeStatus::class
     ];
-
 
     // whenever the rating value is set, make sure it's not above 10
     protected function rating(): Attribute
@@ -29,6 +29,11 @@ class ListItem extends Model
     public function genres(): BelongsToMany
     {
         return $this->belongsToMany(Genre::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 
 }
--- database/migrations/2023_11_18_121933_create_list_items_table.php
+++ database/migrations/2023_11_18_121933_create_list_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('list_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->string('mal_id')->nullable();
             $table->string('status');
